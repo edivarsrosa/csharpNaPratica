09eef76a5fe3f221814c780f4778495102fc0f97
edivarsrosa <edivarsrosa@gmail.com>
2021-04-28 21:01:57 -0300

    tabuada código fonte criado

---
+++ TabuadaCsharp/TabuadaCsharp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TabuadaCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 11; i++)
+            {
+                Console.WriteLine("3 X " + i + " = " + (i*3));
+            }
+            Console.WriteLine();
+            for(int i=0; i<11; i++)
+            {
+                Console.WriteLine("4 X " + i + " = " + (i * 4));
+            }
+        }
+    }
+}
